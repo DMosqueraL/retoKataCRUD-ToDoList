1631032733dc87ebd9f262d7ee819580dd591335
DMosqueral <dml2410@gmail.com>
2022-05-05 14:54:33 -0500

    No era el tipo de mapeo, se usó el mapeo con @Builder y funcionó correctamente levantar el servidor

--- back/src/main/java/co/com/sofka/crud/Mappers/TareaMapper.java
+++ back/src/main/java/co/com/sofka/crud/Mappers/TareaMapper.java
@@ -10,34 +10,34 @@ import org.springframework.web.bind.annotation.Mapping;
 public class TareaMapper {
 
     public Tarea deTareaDtoAEntidad(TareaDto tareaDto) {
-        Tarea tarea = new Tarea();
+        /*Tarea tarea = new Tarea();
         tarea.setId(tareaDto.getId());
         tarea.setName(tareaDto.getName());
         tarea.setCompletado(tareaDto.getCompletado());
         tarea.setIdTareas(tareaDto.getIdTareas());
 
-        return tarea;
-        /*return Tarea.builder()
+        return tarea;*/
+        return Tarea.builder()
                 .id(tareaDto.getId())
                 .name(tareaDto.getName())
                 .completado(tareaDto.getCompletado())
                 .idTareas(tareaDto.getIdTareas())
-                .build();*/
+                .build();
     }
 
     public TareaDto deEntidadADto(Tarea tarea) {
-        TareaDto tareaDto = new TareaDto();
+        /*TareaDto tareaDto = new TareaDto();
         tareaDto.setId(tarea.getId());
         tareaDto.setName(tarea.getName());
         tareaDto.setCompletado(tarea.getCompletado());
         tareaDto.setIdTareas(tarea.getIdTareas());
 
-        return tareaDto;
-        /*return TareaDto.builder()
+        return tareaDto;*/
+        return TareaDto.builder()
                 .id(tarea.getId())
                 .name(tarea.getName())
                 .completado((tarea.getCompletado()))
                 .idTareas(tarea.getIdTareas())
-                .build();*/
+                .build();
     }
 }
--- back/src/main/java/co/com/sofka/crud/Mappers/TodoMapper.java
+++ back/src/main/java/co/com/sofka/crud/Mappers/TodoMapper.java
@@ -9,26 +9,26 @@ import org.springframework.stereotype.Component;
 public class TodoMapper {
 
     public Todo deTodoDtoAEntidad(TodoDto todoDto) {
-        Todo todo = new Todo();
+        /*Todo todo = new Todo();
         todo.setId(todoDto.getId());
         todo.setName(todo.getName());
         todo.setGrupoTareas(todoDto.getGrupoTareas());
 
-        return  todo;
-        /*return Todo.builder().id(todoDto.getId())
-                .name(todoDto.getName()).build();*/
+        return  todo;*/
+        return Todo.builder().id(todoDto.getId())
+                .name(todoDto.getName()).build();
 
     }
 
     public TodoDto deEntidadADto(Todo todo) {
-        TodoDto todoDto = new TodoDto();
+        /*TodoDto todoDto = new TodoDto();
         todoDto.setId(todo.getId());
         todoDto.setName(todo.getName());
         todoDto.setGrupoTareas(todo.getGrupoTareas());
 
-        return todoDto;
-        /*return TodoDto.builder().id(todo.getId())
+        return todoDto;*/
+        return TodoDto.builder().id(todo.getId())
                 .name(todo.getName())
-                .build();*/
+                .build();
     }
 }
